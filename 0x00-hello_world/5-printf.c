5aa4c8b21dbaa6e21a647c174e27feb66222b55e
Arnold <arnoldkisuri7@gmail.com>
2022-10-15 15:37:22 -0700

    number 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * main= entry point for the program
+ * main-entry point for the program
  *
- * Return:0 if no errors, return on zero if errors
+ * Return:0 if no errors, return non zero if errors
  */
 int main(void)
 {
