bfb08d86911e71e9fd96ca0cd77ef3a594a3ac60
Arnold <arnoldkisuri7@gmail.com>
2022-10-16 08:30:23 -0700

    number 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - print lowercase a-z
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char alpha = 'a';
+
+	while (alpha <= 'z')
+	{
+		putchar(alpha);
+		alpa++;
+	}
+	putchar('\n');
+
+	return (0);
+}
