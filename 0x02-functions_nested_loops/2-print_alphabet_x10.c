f8e4f9af273f0137e3dcf574595c1344c02da81f
Arnold <arnoldkisuri7@gmail.com>
2022-10-21 13:52:39 -0700

    number 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -13,10 +13,10 @@ void print_alphabet_x10(void)
 
 	for (counter = 1; counter <= 10; counter++)
 	{
-		for ((alpha = 'a'; alpha <= 'z'; alpha++)
-				{
-				_putchar(alpha);
-				}
-				_putchar('\n');
-				}
-				}
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+		{
+			_putchar(alpha);
+		}
+		_putchar('\n');
+	}
+}
