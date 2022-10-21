8b5771f579f563f6ce5a04eb839129d575787f05
Arnold <arnoldkisuri7@gmail.com>
2022-10-21 13:58:49 -0700

    number 3

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -24,4 +24,12 @@ void print_alphabet(void);
 
 void print_alphabet_x10(void);
 
+/**
+ * _islower - checks for lowercase
+ * @c: character to check
+ * Return: int
+ */
+
+int _islower(int c);
+
 #endif
