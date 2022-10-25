47a39626cca06a6b55e6964715d8558aa33e0a4b
Arnold <arnoldkisuri7@gmail.com>
2022-10-25 05:58:35 -0700

    number 12

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -10,38 +10,27 @@
  **/
 int main(void)
 {
-	char password[84];
-	int index = 0, sum = 0, diff_half1, diff_half2;
+	int sum = 2772;
+	int n;
 
 	srand(time(0));
-
-	while (sum < 2772)
+	while (sum)
 	{
-		password[index] = 33 + rand() % 94;
-		sum += password[index++];
+		n = rand() % 100;
+		 if (n <= 94)
+			 n += 32;
+		 else
+			 continue;
+		 if (sum - n == 0)
+		 {
+			 sum -= n;
+			 printf("%c", n);
+		 }
+		 else if (sum - n - 32 > 0)
+		 {
+			 sum -= n;
+			 printf("%c", n);
+		 }
 	}
-
-	password[index] = '\0';
-
-	if (sum != 2772)
-	{
-		diff_half1 = (sum - 2772) / 2;
-		diff_half2 = (sum - 2772) / 2;
-		if ((sum - 2772) % 2 != 0)
-			diff_half1++;
-
-		for (index = 0; password[index]; index++)
-		{
-			if (password[index] >= (33 + diff_half2))
-			{
-				password[index] -= diff_half2;
-				break;
-			}
-		}
-	}
-
-	printf("%s", password);
-
 	return (0);
 }
-
