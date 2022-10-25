74b5ebb818aec25bf341eb9e17c392ef41c7cdd2
Arnold <arnoldkisuri7@gmail.com>
2022-10-25 07:48:18 -0700

    main

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,103 +1,19 @@
 #ifndef MAIN_H
 #define MAIN_H
-
-/**
- * _putchar - writes the character c to stdout
- * @c:
- * The character to print
- *
- * Return: on success 1.
- * on error, -1 is returned, and error is set appropriately.
- */
+#include <stdio.h>
 
 int _putchar(char c);
-
-/**
- * print_alphabet - print aphabet in lowercase
- */
-
 void print_alphabet(void);
-
-/**
- * print_alphabet_x10 - print lowercase alphabet mara ten
- */
-
 void print_alphabet_x10(void);
-
-/**
- * _islower - checks for lowercase
- * @c: character to check
- * Return: int
- */
-
 int _islower(int c);
-
-/**
- * _isalpha - checks for alphabet character
- * @c: character to check
- * Return: 1 if lower or uppercase, 0 if not alphabet character
- */
-
 int _isalpha(int c);
-
-/**
- * print_sign - print sign of number
- * @n: number to check
- * Return: 1 if greater than 0, 0 if 0, -1 if less than 0
- */
-
 int print_sign(int n);
-
-/**
- * _abs - computer absolute value of integer
- * @int: integer
- * Return: absolute value
- */
-
 int _abs(int);
-
-/**
- * print_last_digit - print last digit of integer
- * @int: integer
- * Return: last digit of number
- */
-
 int print_last_digit(int);
-
-/**
- * jack_bauer - print every minute of a day
- * Description: print minutes 00:00 to 23:59
- */
-
 void jack_bauer(void);
-
-/**
- * times_table - print multiplication table
- */
-
 void times_table(void);
-
-/**
- * add - add two integers
- * @a: first integer
- * @b: second integer
- * Return: sum
- */
-
-int add(int a, int b);
-
-/**
- * print_to_98 - print numbers n to 98
- * @n: integer argument
- */
-
+int add(int, int);
 void print_to_98(int n);
-
-/**
- * print_times_table - Prints the times table of the input,
- * @n: The value of the times table to be printed.
- */
-
-void print_times_table(int n)
+void print_times_table(int n);
 
 #endif
